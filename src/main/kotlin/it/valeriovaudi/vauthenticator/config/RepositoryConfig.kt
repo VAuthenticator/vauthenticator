b2b14a4d0c6888577667f5ae0e4110d3cca2c530
mrFlick72 <valerio.vaudi@gmail.com>
2019-06-18 21:26:45 +0200

    configuration it

--- src/main/kotlin/it/valeriovaudi/vauthenticator/config/RepositoryConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/RepositoryConfig.kt
@@ -2,6 +2,7 @@ package it.valeriovaudi.vauthenticator.config
 
 import it.valeriovaudi.vauthenticator.repository.FileKeyPairRepositoryConfig
 import it.valeriovaudi.vauthenticator.repository.FileKeyRepository
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,5 +15,6 @@ class RepositoryConfig {
     fun fileKeyPairRepositoryConfig() = FileKeyPairRepositoryConfig()
 
     @Bean("keyRepository")
+    @ConditionalOnProperty(value = ["vauthenticator.keypair.repository.type"], havingValue = "FILE_SYSTEM")
     fun fileKeyRepository() = FileKeyRepository(fileKeyPairRepositoryConfig())
 }
--- src/test/kotlin/it/valeriovaudi/vauthenticator/repository/FileKeyPairRepositoryConfigIT.kt
+++ src/test/kotlin/it/valeriovaudi/vauthenticator/repository/FileKeyPairRepositoryConfigIT.kt
@@ -1,15 +1,17 @@
 package it.valeriovaudi.vauthenticator.repository
 
-import org.junit.Assert.*
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 
 @SpringBootTest
 @RunWith(SpringRunner::class)
+@TestPropertySource("classpath:repoconfig.yml")
 class FileKeyPairRepositoryConfigIT {
 
     @Autowired
