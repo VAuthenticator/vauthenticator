32f701a69e34f8f7c31b25ecc359d8fa668efd5a
mrFlick72 <valerio.vaudi@gmail.com>
2019-07-06 17:10:24 +0200

    security account details decoupled from Spring on testable account service

--- src/main/kotlin/it/valeriovaudi/vauthenticator/userdetails/AuthServerAccountServiceBridgePipelineConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/userdetails/AuthServerAccountServiceBridgePipelineConfig.kt
@@ -1,5 +1,6 @@
 package it.valeriovaudi.vauthenticator.userdetails
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,12 +13,18 @@ import org.springframework.integration.dsl.IntegrationFlows.from
 class AuthServerAccountServiceBridgePipelineConfig(private val rabbitTemplate: RabbitTemplate) {
 
     @Bean
-    fun getUserDetailsIntegrationPipelineConfig(authServerAccountServiceBridgeInboundChannel: DirectChannel,
+    fun accountUserDetailsServiceAdapter(objectMapper: ObjectMapper) =
+            AccountUserDetailsServiceAdapter(objectMapper)
+
+    @Bean
+    fun getUserDetailsIntegrationPipelineConfig(accountUserDetailsServiceAdapter: AccountUserDetailsServiceAdapter,
+                                                authServerAccountServiceBridgeInboundChannel: DirectChannel,
                                                 authServerAccountServiceBridgeOutboundChannel: DirectChannel) =
             from(authServerAccountServiceBridgeInboundChannel)
                     .handle<AmqpOutboundEndpoint>(Amqp.outboundGateway(rabbitTemplate)
                             .routingKey("authServerAccountServiceBridgeInboundQueue")
                             .returnChannel(authServerAccountServiceBridgeOutboundChannel))
+                    .transform(accountUserDetailsServiceAdapter, "convert")
                     .get()
 
 }
