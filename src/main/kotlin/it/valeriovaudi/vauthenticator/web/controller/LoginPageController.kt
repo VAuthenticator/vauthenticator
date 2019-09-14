a82b120b3a0e2ab2de8825aad81e9c9650bc3461
mrFlick72 <valerio.vaudi@gmail.com>
2019-09-14 13:48:19 +0200

    openid connect front channel global logout url to logout are now retrieved dynamically from controller

--- src/main/kotlin/it/valeriovaudi/vauthenticator/web/controller/LoginPageController.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/web/controller/LoginPageController.kt
@@ -1,6 +1,7 @@
 package it.valeriovaudi.vauthenticator.web.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 
@@ -12,7 +13,16 @@ class LoginPageController {
 
 
     @GetMapping("/logout/oidc/global_logout")
-    fun forntChannelGlobalLogout() = "/logout/oidc/global_logout"
+    fun forntChannelGlobalLogout(model: Model): String {
+        var federatedServers =
+                listOf("http://localhost:8080/family-budget/logout",
+                        "http://localhost:8080/account/logout",
+                        "http://localhost:8080/vauthenticator/logout"
+                )
+
+        model.addAttribute("federatedServers", federatedServers)
+        return "/logout/oidc/global_logout"
+    }
 
 
 }
