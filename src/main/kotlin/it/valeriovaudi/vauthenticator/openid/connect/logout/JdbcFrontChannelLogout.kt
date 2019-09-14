b9567c130126c4516ccc8ae347243895264d639c
mrFlick72 <valerio.vaudi@gmail.com>
2019-09-15 01:32:54 +0200

    query fix

--- src/main/kotlin/it/valeriovaudi/vauthenticator/openid/connect/logout/JdbcFrontChannelLogout.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/openid/connect/logout/JdbcFrontChannelLogout.kt
@@ -2,7 +2,6 @@ package it.valeriovaudi.vauthenticator.openid.connect.logout
 
 import com.nimbusds.jose.JWSObject
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.query
 import java.sql.ResultSet
 
 interface FrontChannelLogout {
@@ -12,15 +11,25 @@ interface FrontChannelLogout {
 class JdbcFrontChannelLogout(private val jdbcTemplate: JdbcTemplate) : FrontChannelLogout {
 
     private val SELECT_QUERY = "SELECT logout_uris FROM oauth_client_details"
+    private val CHECK_AUD_QUERY = "SELECT count (*) FROM oauth_client_details where client_id=?"
 
     override fun getFederatedLogoutUrls(idTokenHint: String): List<String> {
         val audience = audFor(idTokenHint)
-        val postLogoutRedirectUris = getPostLogoutRedirectUrisFor(audience)
-        return postLogoutRedirectUris.flatMap { it.split(",") }
+        return getPostLogoutRedirectUrisFor(audience)
     }
 
     private fun getPostLogoutRedirectUrisFor(audience: String) =
+            if (isAudienceRegistered(audience)) {
+                logoutUrls().flatMap { it.split(",") }
+            } else {
+                emptyList()
+            }
+
+    private fun logoutUrls(): List<String> =
             jdbcTemplate.query(SELECT_QUERY) { resultSet: ResultSet, _: Int -> resultSet.getString("logout_uris") }
+
+    private fun isAudienceRegistered(audience: String) =
+            jdbcTemplate.queryForObject(CHECK_AUD_QUERY, Int::class.java, audience) > 0
 
     private fun audFor(idTokenHint: String) =
             JWSObject.parse(idTokenHint).payload.toJSONObject().getAsString("aud") as String
