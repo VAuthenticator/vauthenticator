b28ad2fa27c22c2c8a4a11af9af59dbdb0b6ef69
mrFlick72 <valerio.vaudi@gmail.com>
2020-03-05 00:38:49 +0100

    WIP attempt to have more control on token generation

---
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/jwt/SpringJwtEncoder.kt
@@ -0,0 +1,46 @@
+package it.valeriovaudi.vauthenticator.jwt
+
+import org.springframework.security.jwt.JwtHelper
+import org.springframework.security.jwt.crypto.sign.SignatureVerifier
+import org.springframework.security.jwt.crypto.sign.Signer
+import org.springframework.security.oauth2.common.OAuth2AccessToken
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException
+import org.springframework.security.oauth2.common.util.JsonParser
+import org.springframework.security.oauth2.provider.OAuth2Authentication
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter
+import org.springframework.security.oauth2.provider.token.store.JwtClaimsSetVerifier
+
+class SpringJwtEncoder(
+        private val tokenConverter: AccessTokenConverter,
+        private val signer: Signer,
+        private val jwtClaimsSetVerifier: JwtClaimsSetVerifier,
+        private val objectMapper: JsonParser,
+        private val verifier: SignatureVerifier) : JwtEncoder {
+
+    override fun claimsFor(token: String): Map<String, Any?> {
+        return try {
+            val jwt = JwtHelper.decode(token)
+            val claimsStr = jwt.claims
+            val claims = objectMapper.parseMap(claimsStr)
+            if (claims.containsKey(AccessTokenConverter.EXP) && claims[AccessTokenConverter.EXP] is Int) {
+                val intValue = claims[AccessTokenConverter.EXP] as Int?
+                claims[AccessTokenConverter.EXP] = intValue
+            }
+            this.jwtClaimsSetVerifier.verify(claims)
+            claims
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw InvalidTokenException("Cannot convert access token to JSON", e)
+        }
+    }
+
+    override fun encode(accessToken: OAuth2AccessToken, authentication: OAuth2Authentication): String {
+        val content: String
+        try {
+            content = objectMapper.formatMap(tokenConverter.convertAccessToken(accessToken, authentication))
+        } catch (e: java.lang.Exception) {
+            throw IllegalStateException("Cannot convert access token to JSON", e)
+        }
+        return JwtHelper.encode(content, signer).encoded
+    }
+}
