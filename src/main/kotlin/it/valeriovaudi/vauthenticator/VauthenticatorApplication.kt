4003f6949de5f3e8af5593c125254969f4232fef
mrflick72 <valerio.vaudi@gmail.com>
2020-04-09 20:01:43 +0200

    graphite

--- src/main/kotlin/it/valeriovaudi/vauthenticator/VauthenticatorApplication.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/VauthenticatorApplication.kt
@@ -1,8 +1,12 @@
 package it.valeriovaudi.vauthenticator
 
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
 import org.springframework.integration.annotation.IntegrationComponentScan
 import org.springframework.integration.config.EnableIntegration
 
@@ -11,7 +15,13 @@ import org.springframework.integration.config.EnableIntegration
 @EnableIntegration
 @SpringBootApplication
 @IntegrationComponentScan
-class VauthenticatorApplication
+class VauthenticatorApplication {
+    @Bean
+    fun configurer(@Value("\${spring.application.name:}") applicationName: String): MeterRegistryCustomizer<MeterRegistry>? {
+        return MeterRegistryCustomizer { registry: MeterRegistry -> registry.config().commonTags("application", applicationName) }
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<VauthenticatorApplication>(*args)
