a882cde57fdf65876858d71fc06d4c0b23afd165
mrflick72 <valerio.vaudi@gmail.com>
2020-04-26 01:22:57 +0200

    404 page

--- src/main/kotlin/it/valeriovaudi/vauthenticator/account/AccountRegistrationEndPoint.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/account/AccountRegistrationEndPoint.kt
@@ -1,24 +1,29 @@
 package it.valeriovaudi.vauthenticator.account
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 
 
 @Controller
-class AccountRegistrationController(private val accountRegistration: AccountRegistration) {
+class AccountRegistrationController(@Value("\${feature.signup.enabled:false}") private val signupEnabled: Boolean,
+                                    private val accountRegistration: AccountRegistration) {
 
     @GetMapping("/signup")
-    fun view() = "signup"
+    fun view() = if (signupEnabled) "signup" else "error/404"
 
     @GetMapping("/thank-you")
-    fun thankYou() = "thank-you"
+    fun thankYou() = if (signupEnabled) "thank-you" else "error/404"
 
     @PostMapping("/signup")
-    fun signup(account: AccountRepresentation): String {
-        accountRegistration.execute(AccountConverter.fromRepresentationToDomain(account))
-        return "redirect:thank-you"
-    }
+    fun signup(account: AccountRepresentation): String =
+            if (signupEnabled) {
+                accountRegistration.execute(AccountConverter.fromRepresentationToDomain(account))
+                "redirect:thank-you"
+            } else {
+                "redirect:error/404"
+            }
 }
 
 object AccountConverter {
