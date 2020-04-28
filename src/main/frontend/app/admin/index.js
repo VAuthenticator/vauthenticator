10e319506ba4084f8e21fa601c15fe8e52a1ba49
mrflick72 <valerio.vaudi@gmail.com>
2020-04-29 00:54:57 +0200

    first commit admin page hello world

---
+++ src/main/frontend/app/admin/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {withStyles} from "@material-ui/core";
+
+import vauthenticatorStyles from "../component/styles";
+import {HashRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
+import ClientAppManagementPage from "./clientapp/ClientAppManagementPage";
+
+const VAuthenticatorAdminApp = withStyles(vauthenticatorStyles)((props) => {
+    console.log(props)
+    return (
+        <HashRouter>
+            <Switch>
+
+                <Route exact={true} path="/client-application"
+                       render={(props) => <ClientAppManagementPage {...props} />}/>
+            </Switch>
+        </HashRouter>)
+
+})
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<VAuthenticatorAdminApp/>, document.getElementById('app'));
+}
