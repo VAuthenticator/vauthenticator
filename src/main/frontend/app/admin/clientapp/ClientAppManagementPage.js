2bbc5ee902ac95cbde95e36b208f58ba8545dda8
mrflick72 <valerio.vaudi@gmail.com>
2020-05-02 17:48:16 +0200

    get parameter from url

--- src/main/frontend/app/admin/clientapp/ClientAppManagementPage.js
+++ src/main/frontend/app/admin/clientapp/ClientAppManagementPage.js
@@ -4,15 +4,17 @@ import Typography from "@material-ui/core/Typography";
 import {GroupAdd} from "@material-ui/icons";
 import vauthenticatorStyles from "../../component/styles";
 import Template from "../../component/Template";
+import {useParams} from "react-router";
 
 const ClientAppManagementPage = withStyles(vauthenticatorStyles)((props) => {
     const {classes} = props;
-    console.log("OK")
+    let {clientAppId} = useParams();
+
     return (
         <Template maxWidth="lg" classes={classes}>
 
             <Typography variant="h3" component="h3">
-                <GroupAdd fontSize="large"/> VAuthenticator Admin
+                <GroupAdd fontSize="large"/> VAuthenticator Admin {clientAppId}
             </Typography>
 
         </Template>
