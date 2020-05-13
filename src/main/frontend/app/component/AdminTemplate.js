7db26af8470948afbd06dcb4b8b9fce91076b29f
mrflick72 <valerio.vaudi@gmail.com>
2020-05-13 23:47:07 +0200

    logout

--- src/main/frontend/app/component/AdminTemplate.js
+++ src/main/frontend/app/component/AdminTemplate.js
@@ -1,10 +1,11 @@
-import {Container, Paper} from "@material-ui/core";
+import {Container, Link, Paper} from "@material-ui/core";
 import React from "react";
 import Toolbar from "@material-ui/core/Toolbar";
 import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from '@material-ui/icons/Menu';
+import {ExitToApp} from "@material-ui/icons";
 
 export default (props) => {
     const {classes} = props;
@@ -13,12 +14,27 @@ export default (props) => {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon/>
-                    </IconButton>
+                    <Link href="#">
+                        <IconButton edge="start"
+                                    className={classes.menuButton}
+                                    color="inherit"
+                                    aria-label="menu">
+                            <MenuIcon/>
+                        </IconButton>
+                    </Link>
+
                     <Typography variant="h6" className={classes.title}>
                         VAuthenticator Administration
                     </Typography>
+
+                    <Link href="/vauthenticator/logout">
+                        <IconButton edge="start"
+                                    className={classes.menuButton}
+                                    color="inherit"
+                                    aria-label="menu">
+                            <ExitToApp/> Logout
+                        </IconButton>
+                    </Link>
                 </Toolbar>
             </AppBar>
 
