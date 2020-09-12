aa95f0477a5281cd342dbace6701443d99a0dbdb
mrflick72 <valerio.vaudi@gmail.com>
2020-09-13 01:03:36 +0200

    configurations

--- src/main/kotlin/it/valeriovaudi/vauthenticator/config/RepositoryConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/RepositoryConfig.kt
@@ -1,5 +1,7 @@
 package it.valeriovaudi.vauthenticator.config
 
+import it.valeriovaudi.vauthenticator.account.AccountRepository
+import it.valeriovaudi.vauthenticator.account.JdbcAccountRepository
 import it.valeriovaudi.vauthenticator.account.MongoAccountRepository
 import it.valeriovaudi.vauthenticator.extentions.VAuthenticatorPasswordEncoder
 import it.valeriovaudi.vauthenticator.keypair.KeyPairConfig
@@ -7,6 +9,10 @@ import it.valeriovaudi.vauthenticator.keypair.RestKeyRepository
 import it.valeriovaudi.vauthenticator.oauth2.clientapp.JdbcClientApplicationRepository
 import it.valeriovaudi.vauthenticator.openid.connect.userinfo.UserInfoFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,16 +21,13 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.client.RestTemplate
 import javax.sql.DataSource
 
+
 @Configuration
 class RepositoryConfig {
 
     @Bean
-    fun userInfoFactory(accountRepository: MongoAccountRepository) =
+    fun userInfoFactory(accountRepository: AccountRepository) =
             UserInfoFactory(accountRepository)
-
-    @Bean
-    fun accountRepository(mongoTemplate: MongoTemplate) =
-            MongoAccountRepository(mongoTemplate)
 
     @Bean
     fun clientApplicationRepository(dataSource: DataSource, vAuthenticatorPasswordEncoder: VAuthenticatorPasswordEncoder) =
@@ -39,5 +42,27 @@ class RepositoryConfig {
                       @Value("\${repository-service.serviceRegistrationName}") registrationName: String,
                       keyPairConfig: KeyPairConfig): RestKeyRepository =
             RestKeyRepository(RestTemplate(), repositoryServiceUrl, registrationName, keyPairConfig)
+
+}
+
+@Configuration
+@ConditionalOnProperty(prefix = "account.repository", name = ["engine"], havingValue = "database")
+@EnableAutoConfiguration(exclude = [
+    MongoAutoConfiguration::class,
+    MongoDataAutoConfiguration::class]
+)
+class AccountRepositoryWithDatabaseConfig {
+    @Bean
+    fun accountRepository(dataSource: DataSource) =
+            JdbcAccountRepository(JdbcTemplate(dataSource))
+
+}
+
+@Configuration
+@ConditionalOnProperty(prefix = "account.repository", name = ["engine"], havingValue = "mongo")
+class AccountRepositoryWithMongoConfig {
+    @Bean
+    fun accountRepository(mongoTemplate: MongoTemplate) =
+            MongoAccountRepository(mongoTemplate)
 
 }
