bfa6c4abb6d5ac1f358c784d2d59151a528e3841
mrflick72 <valerio.vaudi@gmail.com>
2020-09-13 18:40:39 +0200

    build fix

--- src/main/kotlin/it/valeriovaudi/vauthenticator/account/AccountRepository.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/account/AccountRepository.kt
@@ -1,14 +1,5 @@
 package it.valeriovaudi.vauthenticator.account
 
-import it.valeriovaudi.vauthenticator.account.AccountMapper.fromDocumentToDomain
-import it.valeriovaudi.vauthenticator.account.AccountMapper.fromDomainToDocument
-import org.bson.Document
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.query.Criteria
-import org.springframework.data.mongodb.core.query.Query
-import org.springframework.data.mongodb.core.query.Update
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -90,76 +81,3 @@ class JdbcAccountRepository(private val jdbcTemplate: JdbcTemplate) : AccountRep
     }
 
 }
-
-
-object AccountMapper {
-    fun fromDomainToDocument(account: Account) =
-            Document(mutableMapOf(
-                    "_id" to account.sub,
-
-                    "accountNonExpired" to account.accountNonExpired,
-                    "accountNonLocked" to account.accountNonLocked,
-                    "credentialsNonExpired" to account.credentialsNonExpired,
-                    "enabled" to account.enabled,
-
-                    "username" to account.username,
-                    "password" to account.password,
-                    "authorities" to account.authorities,
-
-                    "sub" to account.sub,
-
-                    "email" to account.email,
-                    "emailVerified" to true,
-
-                    "firstName" to account.firstName,
-                    "lastName" to account.lastName
-            ) as Map<String, Any>?)
-
-
-    fun fromDocumentToDomain(document: Document) =
-            Account(accountNonExpired = document.getBoolean("accountNonExpired"),
-                    accountNonLocked = document.getBoolean("accountNonLocked"),
-                    credentialsNonExpired = document.getBoolean("credentialsNonExpired"),
-                    enabled = document.getBoolean("enabled"),
-
-                    username = document.getString("username"),
-                    password = document.getString("password"),
-                    authorities = document.getList("authorities", String::class.java),
-
-                    email = document.getString("email"),
-                    emailVerified = document.getBoolean("emailVerified"),
-
-                    firstName = document.getString("firstName"),
-                    lastName = document.getString("lastName")
-            )
-}
-
-class MongoAccountRepository(private val mongoTemplate: MongoTemplate) : AccountRepository {
-
-    private val logger: Logger = LoggerFactory.getLogger(MongoAccountRepository::class.java)
-
-    companion object {
-        fun findById(id: String) = Query.query(Criteria.where("_id").`is`(id))
-        fun findByUserName(username: String) = Query.query(Criteria.where("username").`is`(username))
-        const val collectionName = "account"
-    }
-
-    override fun accountFor(username: String): Optional<Account> =
-            Optional.ofNullable(
-                    mongoTemplate.findOne(findByUserName(username), Document::class.java, collectionName)
-            ).map { document -> fromDocumentToDomain(document) }
-
-    override fun save(account: Account) =
-            try {
-                mongoTemplate.upsert(
-                        findByUserName(account.username),
-                        Update.fromDocument(fromDomainToDocument(account)),
-                        collectionName);
-                Unit
-            } catch (e: RuntimeException) {
-                logger.error(e.message, e)
-                throw AccountRegistrationException(e)
-            }
-
-}
-
--- src/main/kotlin/it/valeriovaudi/vauthenticator/config/RepositoryConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/RepositoryConfig.kt
@@ -2,21 +2,15 @@ package it.valeriovaudi.vauthenticator.config
 
 import it.valeriovaudi.vauthenticator.account.AccountRepository
 import it.valeriovaudi.vauthenticator.account.JdbcAccountRepository
-import it.valeriovaudi.vauthenticator.account.MongoAccountRepository
 import it.valeriovaudi.vauthenticator.extentions.VAuthenticatorPasswordEncoder
 import it.valeriovaudi.vauthenticator.keypair.KeyPairConfig
 import it.valeriovaudi.vauthenticator.keypair.RestKeyRepository
 import it.valeriovaudi.vauthenticator.oauth2.clientapp.JdbcClientApplicationRepository
 import it.valeriovaudi.vauthenticator.openid.connect.userinfo.UserInfoFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.client.RestTemplate
 import javax.sql.DataSource
@@ -24,6 +18,10 @@ import javax.sql.DataSource
 
 @Configuration
 class RepositoryConfig {
+
+    @Bean
+    fun accountRepository(dataSource: DataSource) =
+            JdbcAccountRepository(JdbcTemplate(dataSource))
 
     @Bean
     fun userInfoFactory(accountRepository: AccountRepository) =
@@ -42,27 +40,5 @@ class RepositoryConfig {
                       @Value("\${repository-service.serviceRegistrationName}") registrationName: String,
                       keyPairConfig: KeyPairConfig): RestKeyRepository =
             RestKeyRepository(RestTemplate(), repositoryServiceUrl, registrationName, keyPairConfig)
-
-}
-
-@Configuration
-@ConditionalOnProperty(prefix = "account.repository", name = ["engine"], havingValue = "database")
-@EnableAutoConfiguration(exclude = [
-    MongoAutoConfiguration::class,
-    MongoDataAutoConfiguration::class]
-)
-class AccountRepositoryWithDatabaseConfig {
-    @Bean
-    fun accountRepository(dataSource: DataSource) =
-            JdbcAccountRepository(JdbcTemplate(dataSource))
-
-}
-
-@Configuration
-@ConditionalOnProperty(prefix = "account.repository", name = ["engine"], havingValue = "mongo")
-class AccountRepositoryWithMongoConfig {
-    @Bean
-    fun accountRepository(mongoTemplate: MongoTemplate) =
-            MongoAccountRepository(mongoTemplate)
 
 }
