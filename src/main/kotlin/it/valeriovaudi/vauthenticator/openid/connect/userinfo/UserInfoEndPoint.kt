27e3335997877443b69f525b29e84686ed547ae0
mrFlick72 <valerio.vaudi@gmail.com>
2021-04-26 13:55:12 +0200

    nullable obj

--- src/main/kotlin/it/valeriovaudi/vauthenticator/openid/connect/userinfo/UserInfoEndPoint.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/openid/connect/userinfo/UserInfoEndPoint.kt
@@ -1,16 +1,24 @@
 package it.valeriovaudi.vauthenticator.openid.connect.userinfo
 
+import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserInfoEndPoint(private val userInfoFactory: UserInfoFactory) {
 
     @GetMapping("/user-info")
-    fun key(principal: JwtAuthenticationToken) =
-            ok(userInfoFactory.newUserInfo(principal))
+    fun key(
+        principal: JwtAuthenticationToken,
+        @RequestHeader("Authorization") authorization: String
+    ): ResponseEntity<UserInfo> {
+        println("authorization: $authorization")
+        println("principal: $principal")
+        return ok(userInfoFactory.newUserInfo(principal))
+    }
 
 
 }
