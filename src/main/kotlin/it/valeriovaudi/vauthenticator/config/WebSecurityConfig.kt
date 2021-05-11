f1218b7557e24bb8bd84d1549658339955d4773f
mrflick72 <valerio.vaudi@gmail.com>
2021-05-11 23:06:32 +0200

    password encoder

--- src/main/kotlin/it/valeriovaudi/vauthenticator/config/WebSecurityConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/WebSecurityConfig.kt
@@ -2,14 +2,11 @@ package it.valeriovaudi.vauthenticator.config
 
 import it.valeriovaudi.vauthenticator.account.AccountRepository
 import it.valeriovaudi.vauthenticator.security.userdetails.AccountUserDetailsService
-import org.springframework.boot.autoconfigure.security.SecurityProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration.applyDefaultSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
@@ -52,7 +49,7 @@ class WebSecurityConfig {
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
+        return BCryptPasswordEncoder(12)
     }
 
 
