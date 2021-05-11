d0d06610e54df719c37915bb35b9c1d59c02bbe8
mrflick72 <valerio.vaudi@gmail.com>
2021-05-11 20:57:57 +0200

    authorization server updates

--- src/main/kotlin/it/valeriovaudi/vauthenticator/security/registeredclient/ClientAppRegisteredClientRepository.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/security/registeredclient/ClientAppRegisteredClientRepository.kt
@@ -1,6 +1,8 @@
 package it.valeriovaudi.vauthenticator.security.registeredclient
 
-import it.valeriovaudi.vauthenticator.oauth2.clientapp.*
+import it.valeriovaudi.vauthenticator.oauth2.clientapp.ClientAppId
+import it.valeriovaudi.vauthenticator.oauth2.clientapp.ClientApplicationRepository
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.security.oauth2.core.AuthorizationGrantType
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod
@@ -10,57 +12,47 @@ import org.springframework.security.oauth2.server.authorization.config.TokenSett
 import java.time.Duration
 
 class ClientAppRegisteredClientRepository(private val clientApplicationRepository: ClientApplicationRepository) :
-    RegisteredClientRepository {
+        RegisteredClientRepository {
 
-    val logger = LoggerFactory.getLogger(ClientAppRegisteredClientRepository::class.java)
+    val logger: Logger = LoggerFactory.getLogger(ClientAppRegisteredClientRepository::class.java)
 
     override fun save(registeredClient: RegisteredClient) {
-        clientApplicationRepository.save(
-                ClientApplication(
-                        clientAppId = ClientAppId(registeredClient.clientId),
-                        secret = Secret(registeredClient.clientSecret),
-                        accessTokenValidity = TokenTimeToLive(registeredClient.tokenSettings.accessTokenTimeToLive().toSeconds().toInt()),
-                        refreshTokenValidity = TokenTimeToLive(registeredClient.tokenSettings.refreshTokenTimeToLive().toSeconds().toInt()),
-                        additionalInformation = emptyMap(),
-                        authorities = Authorities.empty(),
-
-                )
-        );
+        throw UnsupportedActionException("store client application from ClientAppRegisteredClientRepository is not currently allowed");
     }
 
     override fun findById(id: String): RegisteredClient =
-        registeredClient(id)
+            registeredClient(id)
 
     override fun findByClientId(clientId: String): RegisteredClient =
-        registeredClient(clientId)
+            registeredClient(clientId)
 
 
     private fun registeredClient(id: String) = clientApplicationRepository.findOne(ClientAppId(id))
-        .map { clientApp ->
-            RegisteredClient.withId(id)
-                .clientId(id)
-                .clientSecret(clientApp.secret.content)
-                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
-                .clientAuthenticationMethod(ClientAuthenticationMethod.POST)
-                .authorizationGrantTypes { authorizationGrantTypes ->
-                    authorizationGrantTypes.addAll(clientApp.authorizedGrantTypes.content.map {
-                        AuthorizationGrantType(
-                            it.name.toLowerCase()
-                        )
-                    })
-                }
-                .scopes { scopes -> scopes.addAll(clientApp.scopes.content.map { it.content }) }
-                .redirectUri(clientApp.webServerRedirectUri.content)
-                .tokenSettings { tokenSettings: TokenSettings ->
-                    tokenSettings.accessTokenTimeToLive(Duration.ofSeconds(clientApp.accessTokenValidity.content.toLong()))
-                    tokenSettings.refreshTokenTimeToLive(Duration.ofSeconds(clientApp.refreshTokenValidity.content.toLong()))
-                    tokenSettings.reuseRefreshTokens(true)
-                }
-                .build()
-        }.orElseThrow {
-            logger.error("Application with id or client_id: $id not found")
-            RegisteredClientAppNotFound("Application with id or client_id: $id not found")
-        }
+            .map { clientApp ->
+                RegisteredClient.withId(id)
+                        .clientId(id)
+                        .clientSecret(clientApp.secret.content)
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.POST)
+                        .authorizationGrantTypes { authorizationGrantTypes ->
+                            authorizationGrantTypes.addAll(clientApp.authorizedGrantTypes.content.map {
+                                AuthorizationGrantType(
+                                        it.name.toLowerCase()
+                                )
+                            })
+                        }
+                        .scopes { scopes -> scopes.addAll(clientApp.scopes.content.map { it.content }) }
+                        .redirectUri(clientApp.webServerRedirectUri.content)
+                        .tokenSettings { tokenSettings: TokenSettings ->
+                            tokenSettings.accessTokenTimeToLive(Duration.ofSeconds(clientApp.accessTokenValidity.content.toLong()))
+                            tokenSettings.refreshTokenTimeToLive(Duration.ofSeconds(clientApp.refreshTokenValidity.content.toLong()))
+                            tokenSettings.reuseRefreshTokens(true)
+                        }
+                        .build()
+            }.orElseThrow {
+                logger.error("Application with id or client_id: $id not found")
+                RegisteredClientAppNotFound("Application with id or client_id: $id not found")
+            }
 
 
 }
