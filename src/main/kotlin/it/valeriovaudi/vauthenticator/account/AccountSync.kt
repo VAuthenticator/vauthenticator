cd63a4ebf0f30c7021450fea1305f1f9c71cd61c
mrflick72 <valerio.vaudi@gmail.com>
2021-06-28 19:10:29 +0200

    listener

--- src/main/kotlin/it/valeriovaudi/vauthenticator/account/AccountSync.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/account/AccountSync.kt
@@ -6,19 +6,15 @@ import it.valeriovaudi.aws.messaging.sqs.SqsReactiveListener
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import reactor.core.publisher.Flux
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider
-import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 import java.time.Duration
 import java.util.function.Consumer
 
-private val LOGGER: Logger = LoggerFactory.getLogger(AccountSyncListener::class.java)
+private val LOGGER: Logger = LoggerFactory.getLogger(AccountSyncHandler::class.java)
 
 @Configuration
 class AccountSyncListenerConfig {
@@ -32,48 +28,16 @@ class AccountSyncListenerConfig {
     fun accountSyncListener(@Value("\${vauthenticator.account-sync.listener.sleeping:10m}") sleeping: Duration,
                             receiveMessageRequestFactory: ReceiveMessageRequestFactory,
                             sqsAsyncClient: SqsAsyncClient,
-                            accountSyncHandler: Consumer<String>) =
-            AccountSyncListener(
-                    SqsReactiveListener(
-                            sleeping,
-                            Flux.just(1).repeat(),
-                            receiveMessageRequestFactory,
-                            sqsAsyncClient,
-                            accountSyncHandler
-                    )
-            )
-
-    @Bean
-    fun receiveMessageRequestFactory(@Value("\${vauthenticator.account-sync.listener.queueUrl}") queueUrl: String,
-                                     @Value("\${vauthenticator.account-sync.listener.maxNumberOfMessages}") maxNumberOfMessages: Int,
-                                     @Value("\${vauthenticator.account-sync.listener.visibilityTimeout}") visibilityTimeout: Int,
-                                     @Value("\${vauthenticator.account-sync.listener.waitTimeSeconds}") waitTimeSeconds: Int
-    ): ReceiveMessageRequestFactory {
-        return ReceiveMessageRequestFactory(queueUrl, maxNumberOfMessages, visibilityTimeout, waitTimeSeconds)
-    }
-
-
-    @Bean
-    fun awsCredentialsProvider(): AwsCredentialsProvider {
-        return EnvironmentVariableCredentialsProvider.create()
-    }
-
-
-    @Bean
-    fun sqsAsyncClient(@Value("\${aws.region}") awsRegion: String,
-                       awsCredentialsProvider: AwsCredentialsProvider): SqsAsyncClient {
-        return SqsAsyncClient.builder()
-                .credentialsProvider(awsCredentialsProvider)
-                .region(Region.of(awsRegion))
-                .build()
-    }
-}
-
-class AccountSyncListener(private val listener: SqsReactiveListener) : ApplicationRunner {
-
-    override fun run(args: ApplicationArguments?) {
-        listener.start()
-    }
+                            accountSyncHandler: Consumer<String>): ApplicationRunner =
+            SqsReactiveListener(
+                    sleeping,
+                    Flux.just(1).repeat(),
+                    receiveMessageRequestFactory,
+                    sqsAsyncClient,
+                    accountSyncHandler
+            ).let { listener ->
+                ApplicationRunner { listener.start() }
+            }
 }
 
 class AccountSyncHandler(private val objectMapper: ObjectMapper,
--- src/main/kotlin/it/valeriovaudi/vauthenticator/config/MessagingConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/MessagingConfig.kt
@@ -1,9 +1,38 @@
 package it.valeriovaudi.vauthenticator.config
 
+import it.valeriovaudi.aws.messaging.sqs.ReceiveMessageRequestFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.sqs.SqsAsyncClient
 
 
 @Configuration
 class MessagingConfig {
 
+    @Bean
+    fun awsCredentialsProvider(): AwsCredentialsProvider {
+        return EnvironmentVariableCredentialsProvider.create()
+    }
+
+    @Bean
+    fun sqsAsyncClient(@Value("\${aws.region}") awsRegion: String,
+                       awsCredentialsProvider: AwsCredentialsProvider): SqsAsyncClient {
+        return SqsAsyncClient.builder()
+                .credentialsProvider(awsCredentialsProvider)
+                .region(Region.of(awsRegion))
+                .build()
+    }
+
+    @Bean
+    fun receiveMessageRequestFactory(@Value("\${vauthenticator.account-sync.listener.queueUrl}") queueUrl: String,
+                                     @Value("\${vauthenticator.account-sync.listener.maxNumberOfMessages}") maxNumberOfMessages: Int,
+                                     @Value("\${vauthenticator.account-sync.listener.visibilityTimeout}") visibilityTimeout: Int,
+                                     @Value("\${vauthenticator.account-sync.listener.waitTimeSeconds}") waitTimeSeconds: Int
+    ): ReceiveMessageRequestFactory {
+        return ReceiveMessageRequestFactory(queueUrl, maxNumberOfMessages, visibilityTimeout, waitTimeSeconds)
+    }
 }
