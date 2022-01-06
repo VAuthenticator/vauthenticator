d1c591ecd19b131afdfee0d87b4a21bcd92d24c9
mrflick72 <valerio.vaudi@gmail.com>
2022-01-06 15:03:55 +0100

    clean up dynamo db tables

--- scripts/clean_up_dynamo.py
+++ scripts/clean_up_dynamo.py
@@ -13,9 +13,22 @@ def clean_table_for_primary_key(table_name, key):
             }
         )
 
+def clean_table_for_primary_key_and_sort_ley(table_name, partition_key, sort_key):
+    table = dynamodb.Table(table_name)
+    response = table.scan(ProjectionExpression=f"{partition_key},{sort_key}")
+    items = response.get('Items', [])
+    for item in items:
+        table.delete_item(
+            Key={
+                partition_key: item[partition_key],
+                sort_key: item[sort_key]
+            }
+        )
 
-def clean_account(account_table_name):
+
+def clean_account(account_table_name, account_role_table_name):
     clean_table_for_primary_key(dynamodb, account_table_name, "user_name")
+    clean_table_for_primary_key_and_sort_ley(dynamodb, "%s" % account_role_table_name, "user_name", "role_name")
 
 
 def clean_roles(role_table_name):
@@ -30,8 +43,9 @@ if __name__ == '__main__':
     dynamodb = boto3.resource('dynamodb')
     input_role_table_name = sys.argv[1]
     input_account_table_name = sys.argv[2]
-    input_client_applications_table_name = sys.argv[3]
+    input_account_role_table_name = sys.argv[3]
+    input_client_applications_table_name = sys.argv[4]
 
     clean_roles(dynamodb, input_role_table_name)
-    clean_account(dynamodb, input_account_table_name)
+    clean_account(dynamodb, input_account_table_name, input_account_role_table_name)
     clean_client_applications(dynamodb, input_client_applications_table_name)
