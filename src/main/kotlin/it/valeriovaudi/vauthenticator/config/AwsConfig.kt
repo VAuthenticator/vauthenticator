95aef17a685f281d916d9cb8386b059163e03fd9
mrflick72 <valerio.vaudi@gmail.com>
2022-01-15 13:42:03 +0100

    service account in helm for eks capability

---
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/AwsConfig.kt
@@ -0,0 +1,25 @@
+package it.valeriovaudi.vauthenticator.config
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider
+import java.util.*
+
+class AwsConfig {
+
+    @Bean("awsCredentialsProvider")
+    @ConditionalOnProperty(value = ["iamUserAwsCredentialsProvider"], havingValue = "true")
+    fun iamUserAwsCredentialsProvider(): AwsCredentialsProvider {
+        return EnvironmentVariableCredentialsProvider.create()
+    }
+
+
+    @Bean("awsCredentialsProvider")
+    @ConditionalOnProperty(value = ["awsCredentialsProvider"], havingValue = "true")
+    fun iamServiceAccountAwsCredentialsProvider(): AwsCredentialsProvider {
+        return WebIdentityTokenFileCredentialsProvider.builder().roleSessionName("vauthenticator-${UUID.randomUUID()}").build()
+    }
+
+}
--- src/main/kotlin/it/valeriovaudi/vauthenticator/config/MessagingConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/MessagingConfig.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 
@@ -13,10 +12,6 @@ import software.amazon.awssdk.services.sqs.SqsAsyncClient
 @Configuration
 class MessagingConfig {
 
-    @Bean
-    fun awsCredentialsProvider(): AwsCredentialsProvider {
-        return EnvironmentVariableCredentialsProvider.create()
-    }
 
     @Bean
     fun sqsAsyncClient(@Value("\${aws.region}") awsRegion: String,
