2372a9d5e4e14eb37368772f3988b30679161558
mrflick72 <valerio.vaudi@gmail.com>
2022-04-19 23:44:16 +0200

    refactor ui management movement to another repo

--- management-ui/src/main/frontend/app/component/Separator.js
+++
@@ -1,9 +0,0 @@
-import {Grid} from "@material-ui/core";
-import Divider from "@material-ui/core/Divider";
-import React from "react";
-
-export default function Separator() {
-    return <Grid style={{marginTop: '10px'}}>
-        <Divider/>
-    </Grid>
-}
