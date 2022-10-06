7cd60648ff6451b6aa989f4fa29e7ce2838d9685
mrflick72 <valerio.vaudi@gmail.com>
2022-10-07 01:28:28 +0200

    mail verification config

--- src/main/kotlin/it/valeriovaudi/vauthenticator/mail/MailMessage.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/mail/MailMessage.kt
@@ -3,7 +3,7 @@ package it.valeriovaudi.vauthenticator.mail
 typealias MailContext = Map<String, Any>
 
 enum class MailType(val path: String) {
-    WELCOME("templates/welcome.html"), EMAIL_VERIFICATION("templates/.html"), RESET_PASSWORD("templates/reset-password.html");
+    WELCOME("templates/welcome.html"), EMAIL_VERIFICATION("templates/mail-verify-challenge.html"), RESET_PASSWORD("templates/reset-password.html");
 }
 
 data class MailMessage(val to: String, val from: String, val subject: String, val type: MailType = MailType.WELCOME, val context: MailContext)
