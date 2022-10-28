1b6ebae6fb76833fa14f08318e67a2cab55c5264
mrflick72 <valerio.vaudi@gmail.com>
2022-10-29 00:08:24 +0200

    remove useless object and configs

--- src/main/kotlin/it/valeriovaudi/vauthenticator/config/KeyConfig.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/config/KeyConfig.kt
@@ -1,11 +1,9 @@
 package it.valeriovaudi.vauthenticator.config
 
 import it.valeriovaudi.vauthenticator.keypair.DynamoKeyRepository
-import it.valeriovaudi.vauthenticator.keypair.KeyPairConfig
 import it.valeriovaudi.vauthenticator.keypair.KeyRepository
 import it.valeriovaudi.vauthenticator.keypair.KmsKeyRepository
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
@@ -14,9 +12,6 @@ import java.util.*
 
 @Configuration(proxyBeanMethods = false)
 class KeyConfig {
-    @Bean
-    @ConfigurationProperties(prefix = "key-store")
-    fun keyPairConfig() = KeyPairConfig()
 
     @Bean
     fun keyRepository(kmsClient: KmsClient,
