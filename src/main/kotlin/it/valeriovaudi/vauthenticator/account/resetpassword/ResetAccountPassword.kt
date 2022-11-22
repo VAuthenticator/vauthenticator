051fd18cb2eafaf0f53e622dba069ce35ef324e7
mrflick72 <valerio.vaudi@gmail.com>
2022-11-22 01:56:53 +0100

    reset password from reset password challenge use case bug fix

--- src/main/kotlin/it/valeriovaudi/vauthenticator/account/resetpassword/ResetAccountPassword.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/account/resetpassword/ResetAccountPassword.kt
@@ -5,13 +5,16 @@ import it.valeriovaudi.vauthenticator.account.tiket.InvalidTicketException
 import it.valeriovaudi.vauthenticator.account.tiket.Ticket
 import it.valeriovaudi.vauthenticator.account.tiket.TicketRepository
 import it.valeriovaudi.vauthenticator.account.tiket.VerificationTicket
+import it.valeriovaudi.vauthenticator.password.VAuthenticatorPasswordEncoder
 import java.util.*
 
-class ResetPasswordChallengeSent(
+class ResetAccountPassword(
     private val accountRepository: AccountRepository,
+    private val vAuthenticatorPasswordEncoder: VAuthenticatorPasswordEncoder,
+
     private val ticketRepository: TicketRepository
 ) {
-    fun resetPassword(verificationTicket: VerificationTicket, request: ResetPasswordRequest) {
+    fun resetPasswordFromMailChallenge(verificationTicket: VerificationTicket, request: ResetPasswordRequest) {
         ticketRepository.loadFor(verificationTicket).map {
             passwordResetFor(it, request)
             ticketRepository.delete(verificationTicket)
@@ -21,7 +24,7 @@ class ResetPasswordChallengeSent(
 
     private fun passwordResetFor(it: Ticket, request: ResetPasswordRequest): Optional<Unit> =
         accountRepository.accountFor(it.email).map {
-            val newAccount = it.copy(password = request.newPassword)
+            val newAccount = it.copy(password = vAuthenticatorPasswordEncoder.encode(request.newPassword))
             accountRepository.save(newAccount)
         }
 
