01a88c134c6cbaab30a2388f2e8da778093a5cd1
mrflick72 <valerio.vaudi@gmail.com>
2022-11-22 01:38:45 +0100

    change invalidation reset password ticket according to new password policy check

--- src/main/kotlin/it/valeriovaudi/vauthenticator/account/resetpassword/ResetPasswordChallengeSent.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/account/resetpassword/ResetPasswordChallengeSent.kt
@@ -7,24 +7,22 @@ import it.valeriovaudi.vauthenticator.account.tiket.TicketRepository
 import it.valeriovaudi.vauthenticator.account.tiket.VerificationTicket
 import java.util.*
 
-class ResetPasswordChallengeSent(private val accountRepository: AccountRepository,
-                                 private val ticketRepository: TicketRepository) {
+class ResetPasswordChallengeSent(
+    private val accountRepository: AccountRepository,
+    private val ticketRepository: TicketRepository
+) {
     fun resetPassword(verificationTicket: VerificationTicket, request: ResetPasswordRequest) {
-        invalidateTicketFor(verificationTicket)
-                .map { passwordResetFor(it, request) }
-                .orElseThrow { throw InvalidTicketException("Te ticket ${verificationTicket.content} is not a valid ticket it seems to be used or expired") }
+        ticketRepository.loadFor(verificationTicket).map {
+            passwordResetFor(it, request)
+            ticketRepository.delete(verificationTicket)
+        }
+            .orElseThrow { throw InvalidTicketException("Te ticket ${verificationTicket.content} is not a valid ticket it seems to be used or expired") }
     }
 
-    private fun invalidateTicketFor(verificationTicket: VerificationTicket): Optional<Ticket> =
-            ticketRepository.loadFor(verificationTicket).map { ticket ->
-                ticketRepository.delete(verificationTicket)
-                ticket
-            }
-
     private fun passwordResetFor(it: Ticket, request: ResetPasswordRequest): Optional<Unit> =
-            accountRepository.accountFor(it.email).map {
-                val newAccount = it.copy(password = request.newPassword)
-                accountRepository.save(newAccount)
-            }
+        accountRepository.accountFor(it.email).map {
+            val newAccount = it.copy(password = request.newPassword)
+            accountRepository.save(newAccount)
+        }
 
 }
