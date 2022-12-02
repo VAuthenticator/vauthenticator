64bc51dc012baaf47e05198ba3340136bf2d1adc
mrflick72 <valerio.vaudi@gmail.com>
2022-12-02 11:15:22 +0100

    make the mfa more configurable

---
+++ src/main/frontend/app/mfa/index.js
@@ -0,0 +1,55 @@
+import {Link} from "react-router-dom";
+import {Box, Divider, Grid, ThemeProvider, Typography} from "@mui/material";
+import theme from "../component/styles";
+import Template from "../component/Template";
+import {Fingerprint, Person, VpnKey} from "@mui/icons-material";
+import FormInputTextField from "../component/FormInputTextField";
+import Separator from "../component/Separator";
+import FormButton from "../component/FormButton";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const MfaChallengePage = (props) => {
+    let sendAgainMfaCode = () => {
+        fetch("/mfa-challenge/send", {
+            method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+            credentials: 'same-origin', // include, *same-origin, omit
+        });
+    }
+    return (
+        <ThemeProvider theme={theme}>
+
+            <Template maxWidth="sm">
+                <Typography variant="h3" component="h3">
+                    <VpnKey fontSize="large"/> VAuthenticator MFA module
+                </Typography>
+
+                <Grid style={{marginTop: '10px'}}>
+                    <Divider/>
+                </Grid>
+
+                {<form action="login" method="post">
+                    <Box>
+                        <FormInputTextField id="mfa-code"
+                                            label="mfa-code"
+                                            type="text"
+                                            suffix={<Person fontSize="large"/>}/>
+
+                        <Separator/>
+
+                        <FormButton type="submit" label="Login"/>
+
+                        <FormButton type="button" label="Send again code" onClickHandler={() => sendAgainMfaCode()}/>
+                    </Box>
+                </form>}
+            </Template>
+        </ThemeProvider>
+
+    )
+}
+
+
+if (document.getElementById('app')) {
+    let features = document.getElementById('features').innerHTML
+    ReactDOM.render(<MfaChallengePage rawFeatures={features}/>, document.getElementById('app'));
+}
