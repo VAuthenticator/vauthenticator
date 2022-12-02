95d5716268aa4a33a4d4c54ba795a164d1112be3
mrflick72 <valerio.vaudi@gmail.com>
2022-12-02 01:12:48 +0100

    OTP disagio solved

--- src/main/kotlin/it/valeriovaudi/vauthenticator/mfa/OtpMfa.kt
+++ src/main/kotlin/it/valeriovaudi/vauthenticator/mfa/OtpMfa.kt
@@ -1,6 +1,7 @@
 package it.valeriovaudi.vauthenticator.mfa
 
-import de.taimos.totp.TOTP
+import com.j256.twofactorauth.TimeBasedOneTimePasswordUtil
+import com.j256.twofactorauth.TimeBasedOneTimePasswordUtil.DEFAULT_OTP_LENGTH
 import it.valeriovaudi.vauthenticator.account.Account
 import org.apache.commons.codec.binary.Base32
 import org.apache.commons.codec.binary.Hex
@@ -13,6 +14,8 @@ interface OtpMfa {
 }
 
 class TaimosOtpMfa : OtpMfa {
+    private val tokenTimeWindow: Int = 60 * 10
+    private val tokenTimeWindowMillis: Long = (tokenTimeWindow * 1000).toLong()
 
     override fun generateSecretKeyFor(account: Account): MfaSecret {
         val base32 = Base32()
@@ -23,13 +26,35 @@ class TaimosOtpMfa : OtpMfa {
         val base32 = Base32()
         val bytes = base32.decode(secretKey.content())
         val hexKey: String = Hex.encodeHexString(bytes)
-        return MfaChallenge(TOTP.getOTP(hexKey))
+        return MfaChallenge(
+            TimeBasedOneTimePasswordUtil.generateNumberStringHex(
+                hexKey,
+                System.currentTimeMillis(),
+                tokenTimeWindow,
+                DEFAULT_OTP_LENGTH
+            )
+        )
     }
 
     override fun verify(account: Account, optCode: MfaChallenge) {
-        val secretKey = generateSecretKeyFor(account)
-        val code = getTOTPCode(secretKey)
-        if (optCode != code) {
+        val secretKey = generateSecretKeyFor(account).content()
+        val base32 = Base32()
+        val bytes = base32.decode(secretKey)
+        val hexKey: String = Hex.encodeHexString(bytes)
+        try {
+            val validated =
+                TimeBasedOneTimePasswordUtil.validateCurrentNumberHex(
+                    hexKey,
+                    optCode.content().toInt(),
+                    tokenTimeWindowMillis,
+                    System.currentTimeMillis(),
+                    tokenTimeWindow,
+                    DEFAULT_OTP_LENGTH
+                );
+            if (!validated) {
+                throw MfaException("Customer Code does not match with system code")
+            }
+        } catch (e: RuntimeException) {
             throw MfaException("Customer Code does not match with system code")
         }
     }
