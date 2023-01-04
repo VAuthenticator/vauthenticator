e9e35786dfe91a5934bbb31697308fcb1ff98125
mrflick72 <valerio.vaudi@gmail.com>
2023-01-04 23:25:19 +0100

    make account cacheable

--- src/test/kotlin/it/valeriovaudi/vauthenticator/account/repository/CachedAccountRepositoryTest.kt
+++ src/test/kotlin/it/valeriovaudi/vauthenticator/account/repository/CachedAccountRepositoryTest.kt
@@ -1,18 +1,22 @@
 package it.valeriovaudi.vauthenticator.account.repository
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
+import io.mockk.verify
+import it.valeriovaudi.vauthenticator.account.AccountCacheContentConverter
+import it.valeriovaudi.vauthenticator.account.AccountTestFixture.anAccount
 import it.valeriovaudi.vauthenticator.cache.CacheOperation
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import java.time.Duration
 import java.util.*
 
 @ExtendWith(MockKExtension::class)
 internal class CachedAccountRepositoryTest {
-    private val ttlInSeconds = Duration.ofSeconds(200)
 
     @MockK
     lateinit var delegate: AccountRepository
@@ -20,54 +24,62 @@ internal class CachedAccountRepositoryTest {
     @MockK
     lateinit var cacheOperation: CacheOperation<String, String>
 
+    @MockK
+    lateinit var accountCacheContentConverter: AccountCacheContentConverter
+
     lateinit var underTest: AccountRepository
 
     @BeforeEach
     fun setUp() {
-        underTest = CachedAccountRepository(ObjectMapper(), cacheOperation, delegate)
+        underTest = CachedAccountRepository(accountCacheContentConverter, cacheOperation, delegate)
     }
-    /*
-
-        @Test
-        internal fun `when an account is not found into the cache then it is loaded from database and loaded into the cache`() {
-            val account = anAccount()
-            val username = account.username
-            every { cacheOperation.get(account.email) } returns Optional.empty()
-            every { delegate.accountFor(username) } returns Optional.of(account)
-            every { cacheOperation.put(username, account) } just runs
-
-            underTest.accountFor(username)
-
-            verify { cacheOperation.get(account.email) }
-            verify { delegate.accountFor(username) }
-            verify { cacheOperation.put(username, account) }
-        }
-
-        @Test
-        internal fun `when an account is found from the cache `() {
-            val account = anAccount()
-            val username = account.username
-            every { cacheOperation.get(account.email) } returns Optional.of(account)
-
-            underTest.accountFor(username)
-
-            verify { cacheOperation.get(account.email) }
-            verify(exactly = 0) { delegate.accountFor(username) }
-            verify(exactly = 0) { cacheOperation.put(username, account) }
-        }
 
 
-        @Test
-        fun `when an account is updated`() {
-            val account = anAccount()
-            every { cacheOperation.evict(account.email) } just runs
-            every { delegate.save(account) } just runs
+    @Test
+    internal fun `when an account is not found into the cache then it is loaded from database and loaded into the cache`() {
+        val account = anAccount()
+        val username = account.username
 
-            underTest.save(account)
+        every { cacheOperation.get(account.email) } returns Optional.empty()
+        every { delegate.accountFor(username) } returns Optional.of(account)
+        every { accountCacheContentConverter.loadableContentIntoCacheFor(account) } returns "account"
+        every { cacheOperation.put(username, "account") } just runs
 
-            verify { cacheOperation.evict(account.email) }
-            verify { delegate.save(account) }
-        }
-    */
+        underTest.accountFor(username)
+
+        verify { cacheOperation.get(account.email) }
+        verify { delegate.accountFor(username) }
+        verify { accountCacheContentConverter.loadableContentIntoCacheFor(account) }
+        verify { cacheOperation.put(username, "account") }
+    }
+
+    @Test
+    internal fun `when an account is found from the cache `() {
+        val account = anAccount()
+        val username = account.username
+
+        every { accountCacheContentConverter.getObjectFromCacheContentFor("account") } returns account
+        every { cacheOperation.get(account.email) } returns Optional.of("account")
+
+        underTest.accountFor(username)
+
+        verify { cacheOperation.get(account.email) }
+        verify(exactly = 0) { delegate.accountFor(username) }
+        verify { accountCacheContentConverter.getObjectFromCacheContentFor("account") }
+        verify(exactly = 0) { cacheOperation.put(username, "account") }
+    }
+
+
+    @Test
+    fun `when an account is updated`() {
+        val account = anAccount()
+        every { cacheOperation.evict(account.email) } just runs
+        every { delegate.save(account) } just runs
+
+        underTest.save(account)
+
+        verify { cacheOperation.evict(account.email) }
+        verify { delegate.save(account) }
+    }
 
 }
