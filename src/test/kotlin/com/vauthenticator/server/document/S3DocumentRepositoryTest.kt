a5b6085232bbcc23ea889ca2813cd14f99fed5a9
mrflick72 <valerio.vaudi@gmail.com>
2023-03-23 01:06:21 +0100

    test to fill

--- src/test/kotlin/com/vauthenticator/server/document/S3DocumentRepositoryTest.kt
+++ src/test/kotlin/com/vauthenticator/server/document/S3DocumentRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.vauthenticator.server.document
 
+import com.vauthenticator.server.mail.MailType
 import com.vauthenticator.server.support.DocumentUtils.documentBucket
 import com.vauthenticator.server.support.DocumentUtils.initDocumentTests
 import com.vauthenticator.server.support.DocumentUtils.s3Client
@@ -18,5 +19,20 @@ internal class S3DocumentRepositoryTest {
 
         val expected = FileUtils.loadFileFor("index.html")
         assertEquals(expected, String(document.content))
+    }
+
+    @Test
+    internal fun `store document on S3`() {
+        initDocumentTests(s3Client)
+
+        val documentFile = FileUtils.loadFileFor("index.html")
+        val document = Document("", documentFile.toByteArray())
+
+        val documentRepository = S3DocumentRepository(s3Client, documentBucket)
+
+        documentRepository.saveDocument(MailType.RESET_PASSWORD.name, document)
+
+        val actual =documentRepository.loadDocument("mail", "templates/welcome.html")
+        assertEquals(actual, document)
     }
 }
