a8ed089d33e876e7da4cf28b34b3ccc8b7fece7e
mrflick72 <valerio.vaudi@gmail.com>
2023-05-06 11:31:42 +0200

    default reset page review

--- src/main/frontend/app/reset-password/SuccessfulPasswordReset.js
+++ src/main/frontend/app/reset-password/SuccessfulPasswordReset.js
@@ -6,22 +6,23 @@ import Template from "../component/Template";
 import {VpnKey} from "@mui/icons-material";
 
 const ResetPasswordMainPage = () => {
-    return <ThemeProvider theme={theme}>
-        <Template maxWidth="lg">
-            <Typography variant="h3" component="h3">
-                <VpnKey fontSize="large"/> Reset Password
-            </Typography>
+    return (
+        <ThemeProvider theme={theme}>
+            <Template maxWidth="lg">
+                <Typography variant="h3" component="h3">
+                    <VpnKey fontSize="large"/> Reset Password
+                </Typography>
 
-            <Grid style={{marginTop: '10px'}}>
-                <Divider/>
-            </Grid>
+                <Grid style={{marginTop: '10px'}}>
+                    <Divider/>
+                </Grid>
 
-            <Typography variant="h3" component="h2">
-                Password reset suceeded
-            </Typography>
-        </Template>
-    </ThemeProvider>
-)
+                <Typography variant="h3" component="h2">
+                    Password reset suceeded
+                </Typography>
+            </Template>
+        </ThemeProvider>
+    )
 }
 
 if (document.getElementById('app')) {
