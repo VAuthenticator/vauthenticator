dcb7f4f619aa36284f1c021f8845b659bae59dc2
mrflick72 <valerio.vaudi@gmail.com>
2023-06-13 00:40:18 +0200

    formatting

--- src/main/kotlin/com/vauthenticator/server/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/vauthenticator/server/config/WebSecurityConfig.kt
@@ -98,12 +98,18 @@ class WebSecurityConfig(
             .authorizeHttpRequests { authz ->
                 authz
                     .requestMatchers("/mfa-challenge/send").permitAll()
-                    .requestMatchers("/mfa-challenge").access(mfaAuthorizationManager)
+                    .requestMatchers("/mfa-challenge")
+                    .access(mfaAuthorizationManager)
+
                     .requestMatchers(*WHITE_LIST).permitAll()
                     .requestMatchers("/api/accounts").permitAll()
-                    .requestMatchers(HttpMethod.PUT, "/api/sign-up/mail/welcome").hasAnyAuthority(Scope.WELCOME.content)
+
+                    .requestMatchers(HttpMethod.PUT, "/api/sign-up/mail/welcome")
+                    .hasAnyAuthority(Scope.WELCOME.content)
+
                     .requestMatchers(HttpMethod.PUT, "/api/mail/verify-challenge")
                     .hasAnyAuthority(Scope.MAIL_VERIFY.content)
+
                     .requestMatchers(HttpMethod.PUT, "/api/mail/reset-password-challenge").permitAll()
                     .requestMatchers(HttpMethod.PUT, "/api/reset-password/{ticket}").permitAll()
 
@@ -113,10 +119,17 @@ class WebSecurityConfig(
                     .requestMatchers(HttpMethod.PUT, "/api/mail-template")
                     .hasAnyAuthority(Scope.MAIL_TEMPLATE_WRITER.content)
 
-                    .requestMatchers(HttpMethod.GET, "/api/keys").hasAnyAuthority(Scope.KEY_READER.content)
-                    .requestMatchers(HttpMethod.POST, "/api/keys").hasAnyAuthority(Scope.KEY_EDITOR.content)
-                    .requestMatchers(HttpMethod.DELETE, "/api/keys").hasAnyAuthority(Scope.KEY_EDITOR.content)
-                    .requestMatchers("/api/**").hasAnyAuthority(adminRole)
+                    .requestMatchers(HttpMethod.GET, "/api/keys")
+                    .hasAnyAuthority(Scope.KEY_READER.content)
+
+                    .requestMatchers(HttpMethod.POST, "/api/keys")
+                    .hasAnyAuthority(Scope.KEY_EDITOR.content)
+
+                    .requestMatchers(HttpMethod.DELETE, "/api/keys")
+                    .hasAnyAuthority(Scope.KEY_EDITOR.content)
+
+                    .requestMatchers("/api/**")
+                    .hasAnyAuthority(adminRole)
             }
 
         return http.build()
