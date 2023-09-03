97dcb77c7b6db646b062af923c50337619f86510
mrflick72 <valerio.vaudi@gmail.com>
2023-09-03 21:35:24 +0200

    more components are in typescript

--- src/main/frontend/app/component/FormInputMask.tsx
+++ src/main/frontend/app/component/FormInputMask.tsx
@@ -3,30 +3,42 @@ import PropTypes from 'prop-types';
 import {IMaskInput} from 'react-imask';
 import {Grid, TextField} from "@mui/material";
 
-const InputMask = React.forwardRef(function TextMaskCustom(props, ref) {
+interface CustomProps {
+    onChange: (event: {
+        target: {
+            name: string;
+            value: string
+        }
+    }) => void;
+    name: string;
+}
+
+const InputMask = React.forwardRef<HTMLElement, CustomProps>(
+    function TextMaskCustom(props, ref) {
     const {onChange, ...other} = props;
     return (
         <IMaskInput
             {...other}
-            variant="outlined"
 
             mask="+00 000 0000000"
             definitions={{
                 '#': /[1-9]/,
             }}
-            inputRef={ref}
-            onAccept={(value) => onChange({target: {name: props.name, value}})}
+            onAccept={(value) => props.onChange({target: {name: props.name, value}})}
             overwrite
         />
     );
 });
 
-InputMask.propTypes = {
-    name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-};
+interface FormInputMaskProps {
+    id: string
+    label: string
+    required: boolean
+    value: string
+    handler: (value: any) => void
+}
 
-export default function FormInputMask({id, label, required, value, handler}) {
+const FormInputMask: React.FC<FormInputMaskProps> = ({id, label, required, value, handler}) => {
     return <Grid container spacing={8} alignItems="flex-end">
         <Grid item md={true} sm={true} xs={true}>
             <TextField
@@ -39,9 +51,12 @@ export default function FormInputMask({id, label, required, value, handler}) {
                 name={id}
                 id={id}
                 InputProps={{
-                    inputComponent: InputMask,
+                    inputComponent: InputMask as any,
                 }}
             />
         </Grid>
     </Grid>
 }
+
+
+export default FormInputMask
