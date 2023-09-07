2704f395e8fc2b224043190c4b17d7bcfabeaddd
mrflick72 <valerio.vaudi@gmail.com>
2023-09-08 01:16:56 +0200

    security filter chain bug fix

--- src/main/kotlin/com/vauthenticator/server/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/vauthenticator/server/config/WebSecurityConfig.kt
@@ -110,7 +110,7 @@ class WebSecurityConfig(
                     .requestMatchers(HttpMethod.PUT, "/api/mail/verify-challenge")
                     .hasAnyAuthority(Scope.MAIL_VERIFY.content)
 
-                    .requestMatchers(HttpMethod.PUT, "/api/mail/reset-password-challenge").permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/api/reset-password-challenge").permitAll()
                     .requestMatchers(HttpMethod.PUT, "/api/reset-password/{ticket}").permitAll()
 
                     .requestMatchers(HttpMethod.GET, "/api/mail-template")
