76508e710aa564f2fd3d74bd6345e0b138cb6ffd
mrflick72 <valerio.vaudi@gmail.com>
2023-09-17 23:32:58 +0200

    change password use case

--- src/main/kotlin/com/vauthenticator/server/account/changepassword/ChangePassword.kt
+++ src/main/kotlin/com/vauthenticator/server/account/changepassword/ChangePassword.kt
@@ -1,10 +1,24 @@
 package com.vauthenticator.server.account.changepassword
 
+import com.vauthenticator.server.account.AccountNotFoundException
+import com.vauthenticator.server.account.repository.AccountRepository
+import com.vauthenticator.server.password.PasswordPolicy
+import com.vauthenticator.server.password.VAuthenticatorPasswordEncoder
 import java.security.Principal
 
-class ChangePassword {
-    fun resetPasswordFor(principal: Principal, request: ChangePasswordRequest): Unit {
-        TODO()
+class ChangePassword(
+    private val passwordPolicy: PasswordPolicy,
+    private val passwordEncoder: VAuthenticatorPasswordEncoder,
+    private val accountRepository: AccountRepository
+) {
+    fun resetPasswordFor(principal: Principal, request: ChangePasswordRequest) {
+        passwordPolicy.accept(request.newPassword)
+        accountRepository.accountFor(principal.name)
+            .map { account ->
+                val newEncodedPassword = passwordEncoder.encode(request.newPassword)
+                val updatedAccount = account.copy(password = newEncodedPassword)
+                accountRepository.save(updatedAccount)
+            }.orElseThrow { AccountNotFoundException("account not found") }
     }
 }
 
---
+++ src/test/kotlin/com/vauthenticator/server/account/changepassword/ChangePasswordTest.kt
@@ -0,0 +1,102 @@
+package com.vauthenticator.server.account.changepassword
+
+import com.vauthenticator.server.account.AccountNotFoundException
+import com.vauthenticator.server.account.AccountTestFixture.anAccount
+import com.vauthenticator.server.account.EMAIL
+import com.vauthenticator.server.account.repository.AccountRepository
+import com.vauthenticator.server.clientapp.A_CLIENT_APP_ID
+import com.vauthenticator.server.oauth2.clientapp.ClientAppId
+import com.vauthenticator.server.oauth2.clientapp.Scope
+import com.vauthenticator.server.password.PasswordPolicy
+import com.vauthenticator.server.password.VAuthenticatorPasswordEncoder
+import com.vauthenticator.server.support.SecurityFixture.principalFor
+import com.vauthenticator.server.support.VAUTHENTICATOR_ADMIN
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.web.servlet.MockMvc
+import java.util.*
+
+@ExtendWith(MockKExtension::class)
+internal class ChangePasswordTest {
+
+    lateinit var mokMvc: MockMvc
+
+    val clientAppId = ClientAppId(A_CLIENT_APP_ID)
+
+
+    private lateinit var underTest: ChangePassword
+
+    @MockK
+    lateinit var passwordPolicy: PasswordPolicy
+
+    @MockK
+    lateinit var accountRepository: AccountRepository
+
+    @MockK
+    lateinit var passwordEncoder: VAuthenticatorPasswordEncoder
+
+    @BeforeEach
+    internal fun setUp() {
+        underTest = ChangePassword(
+            passwordPolicy,
+            passwordEncoder,
+            accountRepository
+        )
+    }
+
+    @Test
+    internal fun `when a change password attempt is executed`() {
+        val account = anAccount()
+        val principal = principalFor(
+            A_CLIENT_APP_ID,
+            EMAIL,
+            listOf(VAUTHENTICATOR_ADMIN),
+            listOf(Scope.RESET_PASSWORD.content)
+        )
+
+        every { passwordPolicy.accept("it is a new password") } just runs
+        every { accountRepository.accountFor(principal.name) } returns Optional.of(account)
+        every { passwordEncoder.encode("it is a new password") } returns "it is a encoded new password"
+        every { accountRepository.save(account.copy(password = "it is a encoded new password")) } just runs
+
+        underTest.resetPasswordFor(principal, ChangePasswordRequest("it is a new password"))
+
+        verify { passwordPolicy.accept("it is a new password") }
+        verify { accountRepository.accountFor(principal.name) }
+        verify { passwordEncoder.encode("it is a new password") }
+        verify { accountRepository.save(account.copy(password = "it is a encoded new password")) }
+    }
+
+    @Test
+    internal fun `when a user is not found`() {
+        val principal = principalFor(
+            A_CLIENT_APP_ID,
+            EMAIL,
+            listOf(VAUTHENTICATOR_ADMIN),
+            listOf(Scope.RESET_PASSWORD.content)
+        )
+
+        every { passwordPolicy.accept("it is a new password") } just runs
+        every { accountRepository.accountFor(principal.name) } returns Optional.empty()
+
+
+        assertThrows(AccountNotFoundException::class.java) {
+            underTest.resetPasswordFor(
+                principal,
+                ChangePasswordRequest("it is a new password")
+            )
+        }
+
+        verify { passwordPolicy.accept("it is a new password") }
+        verify { accountRepository.accountFor(principal.name) }
+    }
+
+}
