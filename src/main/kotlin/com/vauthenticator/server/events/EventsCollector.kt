1c69a510790aec4f555a3e408540139eb12f1c2f
mrflick72 <valerio.vaudi@gmail.com>
2023-10-06 22:18:51 +0200

    debug logging

--- src/main/kotlin/com/vauthenticator/server/events/EventsCollector.kt
+++ src/main/kotlin/com/vauthenticator/server/events/EventsCollector.kt
@@ -1,15 +1,18 @@
 package com.vauthenticator.server.events
 
+import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
 
 class SpringEventsCollector(private val eventConsumers: List<EventConsumer>) : EventsCollector {
 
+    private val logger = LoggerFactory.getLogger(SpringEventsCollector::class.java)
     @EventListener
     override fun accept(event: VAuthenticatorEvent) {
+        logger.debug("event ${event::class.simpleName} is collected")
         eventConsumers.forEach {
-            println(it::class.java)
-            println(it.handleable(event))
-            if(it.handleable(event)){
+            val handleable = it.handleable(event)
+            logger.debug("event ${event::class.simpleName} is handleable $handleable")
+            if(handleable){
                 it.accept(event)
             }
         }
