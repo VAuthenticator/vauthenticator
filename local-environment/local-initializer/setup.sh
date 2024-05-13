ec1da25204bfaf6b3216840e29ce9a939ce9c187
mrflick72 <valerio.vaudi@gmail.com>
2024-05-13 11:58:09 +0200

    [skip ci] script fix

--- local-environment/local-initializer/setup.sh
+++ local-environment/local-initializer/setup.sh
@@ -1,49 +1,32 @@
-function copy_tf_variables() {
-  echo $ACCOUNT_ID
-  sed 's/ACCOUNT_ID/'$ACCOUNT_ID'/g' ../../local-environment/variables.tfvars | sed 's/VAUTHENTICATOR_BUCKET/'$VAUTHENTICATOR_BUCKET'/g' | sed 's/VAUTHENTICATOR_MANAGEMENT_UI_BUCKET/'$VAUTHENTICATOR_MANAGEMENT_UI_BUCKET'/g' > variables.tfvars
-
-}
-
-function create_symbolic_linkFor() {
+function prepare_environment_for() {
   cd $1
-  ln -sf ../variable.tf variable.tf
-  cp -f ../../local-environment/terraform.tf terraform.tf
+  ln -sf ../../../iac/terraform/variable.tf  variable.tf
+  cp -f ../../local-initializer/terraform.tf terraform.tf
+  cp -f ../../local-initializer/variables.tfvars variables.tfvars
   cd ..
 }
 
 source .env
 
-mkdir local-tenant-iac
-cd local-tenant-iac
-c
+mkdir ../local-tenant-iac
+cd ../local-tenant-iac
+
 cp -R ../../iac/terraform/iam .
 cp -R ../../iac/terraform/resources .
 rm resources/s3.tf
 
 
-create_symbolic_linkFor iam
-create_symbolic_linkFor resources
-create_symbolic_linkFor policy
+prepare_environment_for iam
+prepare_environment_for resources
 
 # IAM
 cd iam
-copy_tf_variables
-
-terraform init -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="region=$AWS_REGION"
+terraform init
 terraform plan -var-file=./variables.tfvars
 terraform apply --auto-approve -var-file=variables.tfvars
+
 #
 cd ../resources
-copy_tf_variables
-
-terraform init -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="region=$AWS_REGION"
+terraform init
 terraform plan -var-file=variables.tfvars
 terraform apply -var-file=variables.tfvars -auto-approve
-
-cd ../policy
-copy_tf_variables
-
-terraform init -backend-config="bucket=$TF_STATE_BUCKET" -backend-config="region=$AWS_REGION"
-terraform plan -var-file=variables.tfvars
-terraform apply -var-file=variables.tfvars -auto-approve
-
--- local-environment/local-initializer/variables.tfvars
+++ local-environment/local-initializer/variables.tfvars
@@ -9,13 +9,13 @@ path = "/local-stage/"
 table_name_suffix  = "_Local_Staging"
 
 # s3 variables
-vauthenticator_document_s3_bucket_name = "VAUTHENTICATOR_BUCKET"
-vauthenticator_management_ui_document_s3_bucket_name = "VAUTHENTICATOR_MANAGEMENT_UI_BUCKET"
+vauthenticator_document_s3_bucket_name = "irrelevant"
+vauthenticator_management_ui_document_s3_bucket_name = "irrelevant"
 
 # kms variables
 key_name                      = "master_key"
-key_administrator_account_ids = ["arn:aws:iam::ACCOUNT_ID:user/local-stage/vauthenticator-local-dev"]
-key_user_account_ids = ["arn:aws:iam::ACCOUNT_ID:user/local-stage/vauthenticator-local-dev"]
+key_administrator_account_ids = ["arn:aws:iam::000000000000:user/local-stage/vauthenticator-local-dev"]
+key_user_account_ids = ["arn:aws:iam::000000000000:user/local-stage/vauthenticator-local-dev"]
 
 key_alias               = "vauthenticator-local-dev-key"
 key_description         = "vauthenticator-local-dev-key"
