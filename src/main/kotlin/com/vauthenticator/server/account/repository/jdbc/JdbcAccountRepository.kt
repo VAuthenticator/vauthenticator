00ef5dc6ff97b5575365fa574fd608c6b9f2445a
mrflick72 <valerio.vaudi@gmail.com>
2024-06-03 23:11:16 +0200

    WIP JDBC repository

--- src/main/kotlin/com/vauthenticator/server/account/repository/jdbc/JdbcAccountRepository.kt
+++ src/main/kotlin/com/vauthenticator/server/account/repository/jdbc/JdbcAccountRepository.kt
@@ -1,20 +1,177 @@
 package com.vauthenticator.server.account.repository.jdbc
 
-import com.vauthenticator.server.account.Account
+import com.vauthenticator.server.account.*
+import com.vauthenticator.server.account.Date
+import com.vauthenticator.server.account.repository.AccountRegistrationException
 import com.vauthenticator.server.account.repository.AccountRepository
+import org.springframework.dao.DuplicateKeyException
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
+private const val SAVE_QUERY: String = """
+        INSERT INTO Account (
+            account_non_expired,
+            account_non_locked,
+            credentials_non_expired,
+            enabled,
+            username,
+            password,
+            email,
+            email_verified,
+            first_name,
+            last_name,
+            birth_date,
+            phone,
+            locale,
+            mandatory_action
+        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)  ON CONFLICT(username) DO UPDATE SET account_non_expired=?,
+            account_non_locked=?,
+            credentials_non_expired=?,
+            enabled=?,
+            password=?,
+            email=?,
+            email_verified=?,
+            first_name=?,
+            last_name=?,
+            birth_date=?,
+            phone=?,
+            locale=?,
+            mandatory_action=?
+    """
+private const val CREATE_QUERY: String = """
+        INSERT INTO Account (
+            account_non_expired,
+            account_non_locked,
+            credentials_non_expired,
+            enabled,
+            username,
+            password,
+            email,
+            email_verified,
+            first_name,
+            last_name,
+            birth_date,
+            phone,
+            locale,
+            mandatory_action
+        ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?) 
+    """
 
-class JdbcAccountRepository : AccountRepository{
+private const val FIND_ONE_QUERY: String = """
+    SELECT account_non_expired,
+           account_non_locked,
+           credentials_non_expired,
+           enabled,
+           username,
+           password,
+           email,
+           email_verified,
+           first_name,
+           last_name,
+           birth_date,
+           phone,
+           locale,
+           mandatory_action
+     FROM Account
+     WHERE username=?
+    """
+
+private const val FIND_ACCOUNT_ROLE_QUERY: String = """
+    SELECT role_name
+     FROM ACCOUNT_ROLE
+     WHERE account_username=?
+    """
+
+
+@Transactional
+class JdbcAccountRepository(private val jdbcTemplate: JdbcTemplate) : AccountRepository {
+
+    @Transactional(readOnly = true)
     override fun accountFor(username: String): Optional<Account> {
-        TODO("Not yet implemented")
+
+        val authorities: Set<String> =
+            jdbcTemplate.query(FIND_ACCOUNT_ROLE_QUERY, { rs, i -> rs.getString("role_name") }, username).toSet()
+
+        val queryResult = jdbcTemplate.query(FIND_ONE_QUERY, { rs, i ->
+            Account(
+                accountNonExpired = rs.getBoolean("account_non_expired"),
+                accountNonLocked = rs.getBoolean("account_non_locked"),
+                credentialsNonExpired = rs.getBoolean("credentials_non_expired"),
+                enabled = rs.getBoolean("enabled"),
+                username = rs.getString("username"),
+                password = rs.getString("password"),
+                email = rs.getString("email"),
+                emailVerified = rs.getBoolean("email_verified"),
+                firstName = rs.getString("first_name"),
+                lastName = rs.getString("last_name"),
+                authorities = authorities,
+                birthDate = Date.isoDateFor(rs.getString("birth_date")),
+                phone = Phone.phoneFor(rs.getString("phone")),
+                locale = UserLocale.localeFrom(rs.getString("locale")),
+                mandatoryAction = AccountMandatoryAction.valueOf(rs.getString("mandatory_action"))
+            )
+        }, username)
+        return Optional.ofNullable(queryResult.firstOrNull())
     }
 
     override fun save(account: Account) {
-        TODO("Not yet implemented")
+        jdbcTemplate.update(
+            SAVE_QUERY,
+            account.accountNonExpired,
+            account.accountNonLocked,
+            account.accountNonExpired,
+            account.enabled,
+            account.username,
+            account.password,
+            account.email,
+            account.emailVerified,
+            account.firstName,
+            account.lastName,
+            account.birthDate.map { it.formattedDate() }.orElse(null),
+            account.phone.map { it.formattedPhone() }.orElse(""),
+            account.locale.map { it.formattedLocale() }.orElse(""),
+            account.mandatoryAction.name,
+
+
+            account.accountNonExpired,
+            account.accountNonLocked,
+            account.accountNonExpired,
+            account.enabled,
+            account.password,
+            account.email,
+            account.emailVerified,
+            account.firstName,
+            account.lastName,
+            account.birthDate.map { it.formattedDate() }.orElse(null),
+            account.phone.map { it.formattedPhone() }.orElse(""),
+            account.locale.map { it.formattedLocale() }.orElse(""),
+            account.mandatoryAction.name
+        )
     }
 
     override fun create(account: Account) {
-        TODO("Not yet implemented")
+        try {
+            jdbcTemplate.update(
+                CREATE_QUERY,
+                account.accountNonExpired,
+                account.accountNonLocked,
+                account.accountNonExpired,
+                account.enabled,
+                account.username,
+                account.password,
+                account.email,
+                account.emailVerified,
+                account.firstName,
+                account.lastName,
+                account.birthDate.map { it.formattedDate() }.orElse(null),
+                account.phone.map { it.formattedPhone() }.orElse(""),
+                account.locale.map { it.formattedLocale() }.orElse(""),
+                account.mandatoryAction.name
+            )
+        } catch (e: DuplicateKeyException) {
+            throw AccountRegistrationException("", e)
+        }
+
     }
 }
--- src/test/kotlin/com/vauthenticator/server/account/repository/jdbc/JdbcAccountRepositoryTest.kt
+++ src/test/kotlin/com/vauthenticator/server/account/repository/jdbc/JdbcAccountRepositoryTest.kt
@@ -3,11 +3,20 @@ package com.vauthenticator.server.account.repository.jdbc
 import com.vauthenticator.server.account.repository.AbstractAccountRepositoryTest
 import com.vauthenticator.server.account.repository.AccountRepository
 import com.vauthenticator.server.role.RoleRepository
+import com.vauthenticator.server.role.protectedRoleNames
+import com.vauthenticator.server.role.repository.jdbc.JdbcRoleRepository
+import com.vauthenticator.server.support.JdbcUtils.jdbcTemplate
+import com.vauthenticator.server.support.JdbcUtils.resetDb
 
 class JdbcAccountRepositoryTest : AbstractAccountRepositoryTest() {
 
-    override fun initAccountRepository(roleRepository: RoleRepository): AccountRepository = TODO()
-    override fun initRoleRepository(): RoleRepository = TODO()
-    override fun resetDatabase() = TODO()
+    override fun initAccountRepository(roleRepository: RoleRepository): AccountRepository =
+        JdbcAccountRepository(jdbcTemplate)
+
+    override fun initRoleRepository(): RoleRepository = JdbcRoleRepository(jdbcTemplate, protectedRoleNames)
+
+    override fun resetDatabase() {
+        resetDb()
+    }
 
 }
