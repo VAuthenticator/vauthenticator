2631862ff4924440aa21da07dcac96113101fbfa
mrflick72 <valerio.vaudi@gmail.com>
2024-06-23 16:47:56 +0200

    missed commit

--- local-environment/local-initializer/setup.py
+++ local-environment/local-initializer/setup.py
@@ -129,7 +129,7 @@ def store_client_applications():
         "with_pkce": False,
         "scopes": set([
             "openid", "profile", "email",
-            "admin:signup", "admin:welcome", "admin:mail-verify", "admin:reset-password", "admin:change-password",
+            "admin:signup", "admin:welcome", "admin:email-verify", "admin:reset-password", "admin:change-password",
             "admin:key-reader", "admin:key-editor",
             "admin:mail-template-reader", "admin:mail-template-writer",
             "mfa:always"
