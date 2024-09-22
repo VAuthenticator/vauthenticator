130dab690105d65473f72bece0b1678c82b80a1c
mrflick72 <valerio.vaudi@gmail.com>
2024-09-22 15:53:55 +0200

    sns policy fix

--- iac/terraform/policy/sns.tf
+++ iac/terraform/policy/sns.tf
@@ -1,10 +1,5 @@
 data "aws_iam_policy_document" "vauthenticator_sns_send_sms_policy" {
   statement {
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_iam_user.vauthenticator.arn]
-    }
-
     actions = [
       "sns:Publish"
     ]
