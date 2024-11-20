a544c409e2b678881c7578206d38faa06d2bd7a9
mrflick72 <valerio.vaudi@gmail.com>
2024-11-20 22:02:24 +0100

    revert useless class

--- src/main/kotlin/com/vauthenticator/server/job/DatabaseTtlEntryCleanJob.kt
+++
@@ -1,6 +0,0 @@
-package com.vauthenticator.server.job
-
-class DatabaseTtlEntryCleanJob {
-
-
-}
--- src/test/kotlin/com/vauthenticator/server/job/DatabaseTtlEntryCleanJobTest.kt
+++
@@ -1,8 +0,0 @@
-package com.vauthenticator.server.job
-
-import org.junit.jupiter.api.Assertions.*
-
-class DatabaseTtlEntryCleanJobTest {
-
-
-}
