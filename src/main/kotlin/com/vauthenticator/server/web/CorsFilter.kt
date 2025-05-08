bd60cdcf29eb3da61776b7ab09d7650114d32a91
mrflick72 <valerio.vaudi@gmail.com>
2025-05-08 17:02:21 +0200

    token endpoint processing when the client id is provided via request param

--- src/main/kotlin/com/vauthenticator/server/web/CorsFilter.kt
+++ src/main/kotlin/com/vauthenticator/server/web/CorsFilter.kt
@@ -1,16 +1,26 @@
 package com.vauthenticator.server.web
 
+import com.vauthenticator.server.oauth2.clientapp.domain.ClientAppId
+import com.vauthenticator.server.oauth2.clientapp.domain.ClientApplicationRepository
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.web.filter.OncePerRequestFilter
 
-class CorsFilter : OncePerRequestFilter() {
+class CorsFilter(private val clientApplicationRepository: ClientApplicationRepository) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        TODO("Not yet implemented")
+        val clientId = request.parameterMap["client_id"]?.first()
+        if (clientId != null && !clientApplicationRepository.findOne(ClientAppId(clientId)).isEmpty) {
+            request.remoteHost.let { response.addHeader("Access-Control-Allow-Origin", it) }
+            response.addHeader("Access-Control-Allow-Methods", "GET POST PUT DELETE OPTION")
+            response.addHeader("Access-Control-Max-Age", "3600")
+            response.addHeader("Access-Control-Allow-Credentials", "true")
+        }
+
+        filterChain.doFilter(request, response)
     }
 }
--- src/test/kotlin/com/vauthenticator/server/web/CorsFilterTest.kt
+++ src/test/kotlin/com/vauthenticator/server/web/CorsFilterTest.kt
@@ -1,7 +1,46 @@
 package com.vauthenticator.server.web
 
+import com.vauthenticator.server.oauth2.clientapp.domain.ClientApplicationRepository
+import com.vauthenticator.server.support.ClientAppFixture.aClientApp
+import com.vauthenticator.server.support.ClientAppFixture.aClientAppId
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.mock.web.MockFilterChain
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpServletResponse
+import java.util.Optional
 
+@ExtendWith(MockKExtension::class)
 class CorsFilterTest {
 
+    @MockK
+    lateinit var clientApplicationRepository: ClientApplicationRepository
+
+    @Test
+    fun `when the client app aware endpoints are allowed`() {
+        val clientAppId = aClientAppId()
+        every { clientApplicationRepository.findOne(clientAppId) } returns Optional.of(aClientApp(clientAppId = clientAppId))
+
+        val uut = CorsFilter(clientApplicationRepository)
+
+        val request = MockHttpServletRequest()
+        request.method = "POST"
+        request.requestURI = "/token"
+        request.remoteHost = "example.com"
+        request.addParameter("client_id", clientAppId.content)
+
+        val response = MockHttpServletResponse()
+        val filterChain = MockFilterChain()
+
+        uut.doFilter(request, response, filterChain)
+
+        assertTrue { response.getHeaders("Access-Control-Allow-Origin").contains("example.com") }
+        assertTrue { response.getHeaders("Access-Control-Allow-Methods").contains("GET POST PUT DELETE OPTION") }
+        assertTrue { response.getHeaders("Access-Control-Max-Age").contains("3600") }
+        assertTrue { response.getHeaders("Access-Control-Allow-Credentials").contains("true") }
+    }
 }
