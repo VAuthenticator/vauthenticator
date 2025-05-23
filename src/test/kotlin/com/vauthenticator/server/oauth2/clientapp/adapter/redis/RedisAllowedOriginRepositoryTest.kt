bddc4fc5ab252bc9264d5762ce42a9c433309d31
mrflick72 <valerio.vaudi@gmail.com>
2025-05-23 22:55:53 +0200

    approach allowed origins redis implementation

--- src/test/kotlin/com/vauthenticator/server/oauth2/clientapp/adapter/redis/RedisAllowedOriginRepositoryTest.kt
+++ src/test/kotlin/com/vauthenticator/server/oauth2/clientapp/adapter/redis/RedisAllowedOriginRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.vauthenticator.server.oauth2.clientapp.adapter.redis
 
+import com.vauthenticator.server.extentions.toSha256
 import com.vauthenticator.server.oauth2.clientapp.adapter.AbstractAllowedOriginRepositoryTest
 import com.vauthenticator.server.oauth2.clientapp.domain.AllowedOriginRepository
 import com.vauthenticator.server.support.RedisUtils
@@ -7,8 +8,23 @@ import com.vauthenticator.server.support.RedisUtils
 class RedisAllowedOriginRepositoryTest : AbstractAllowedOriginRepositoryTest() {
     val redisTemplate = RedisUtils.aRedisTemplate()
 
+
     override fun resetDatabase() {
-//        redisTemplate.opsForList().
+        val opsForHash = redisTemplate.opsForHash<String, String>()
+        opsForHash.delete("allowed_origin.ONE_CLIENT_APP_ID", "allowed_origin.ONE_CLIENT_APP_ID".toSha256())
+        opsForHash.delete("allowed_origin.ANOTHER_CLIENT_APP_ID", "allowed_origin.ANOTHER_CLIENT_APP_ID".toSha256())
+
+        opsForHash.put(
+            "allowed_origin.ONE_CLIENT_APP_ID",
+            "allowed_origin.ANOTHER_CLIENT_APP_ID".toSha256(),
+            "http://localhost:8080"
+        )
+        opsForHash.put(
+            "allowed_origin.ANOTHER_CLIENT_APP_ID",
+            "allowed_origin.ANOTHER_CLIENT_APP_ID".toSha256(),
+            "http://localhost:9090"
+        )
+
     }
 
     override fun initUnitUnderTest(): AllowedOriginRepository {
