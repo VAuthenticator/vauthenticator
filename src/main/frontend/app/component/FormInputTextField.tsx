d181a78ad93c9e2a50a899732f559a8a7135d8a9
mrflick72 <valerio.vaudi@gmail.com>
2025-10-30 22:17:15 +0100

    ui fix

--- src/main/frontend/app/component/FormInputTextField.tsx
+++ src/main/frontend/app/component/FormInputTextField.tsx
@@ -1,31 +1,57 @@
-import {Grid, TextField} from "@mui/material";
-import React, {ChangeEventHandler} from "react";
+import { Grid, InputAdornment, TextField } from "@mui/material";
+import React, { ChangeEventHandler } from "react";
 
 interface FormInputTextFieldProps {
-    id: string
-    label: string
-    type?: string
-    required?:boolean
-    autoFocus?: boolean
-    disabled?: boolean
-    suffix?: any
-    value?: string
-    handler?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
+  id: string;
+  label: string;
+  type?: string;
+  required?: boolean;
+  autoFocus?: boolean;
+  disabled?: boolean;
+  suffix?: any;
+  value?: string;
+  handler?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
 }
 
-const FormInputTextField: React.FC<FormInputTextFieldProps>
-= ({id, label, type, required, autoFocus, disabled, suffix, value, handler}) => {
-    return <Grid container spacing={8} alignItems="flex-end">
-        {suffix && <Grid>
-            {suffix}
-        </Grid>}
-        <Grid size={{ xs: 12 }}>
-            <TextField name={id} id={id} label={label} type={type || "text"} disabled={disabled}
-                       variant="outlined" fullWidth autoFocus={autoFocus} required={required || false}
-                       value={value}
-                       onChange={handler}/>
-        </Grid>
+const FormInputTextField: React.FC<FormInputTextFieldProps> = ({
+  id,
+  label,
+  type,
+  required,
+  autoFocus,
+  disabled,
+  suffix,
+  value,
+  handler,
+}) => {
+  return (
+    <Grid container spacing={8} alignItems="flex-end">
+      <Grid size={{ xs: 12 }}>
+        <TextField
+          name={id}
+          id={id}
+          label={label}
+          type={type || "text"}
+          disabled={disabled}
+          variant="outlined"
+          fullWidth
+          autoFocus={autoFocus}
+          required={required || false}
+          value={value}
+          onChange={handler}
+          slotProps={{
+            input: suffix
+              ? {
+                  startAdornment: (
+                    <InputAdornment position="start">{suffix}</InputAdornment>
+                  ),
+                }
+              : undefined,
+          }}
+        />
+      </Grid>
     </Grid>
-}
+  );
+};
 
-export default FormInputTextField
+export default FormInputTextField;
